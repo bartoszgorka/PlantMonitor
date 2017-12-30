b4d4bd8d6c40ba3a50d954df00d47a4e3fd0e0bf
Bartosz Górka <bartosz.gorka@appunite.com>
2017-12-30 20:37:20 +0100

    Private & Public RSA key

--- apps/plant_monitor/config/prod.exs
+++ apps/plant_monitor/config/prod.exs
@@ -4,3 +4,7 @@ config :plant_monitor, PlantMonitor.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: "${DATABASE_URL}",
   pool_size: 10
+
+config :plant_monitor,
+  private_key: "${PRIVATE_KEY}",
+  public_key: "${PUBLIC_KEY}"
